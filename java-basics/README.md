4a640229044f5ce0e08796f5f3bc294adae7bf45
MohanNannivala <mohannannivala@yahoo.com>
2023-02-26 22:34:54 +0530

    add readme
    Signed-off-by: MohanNannivala <mohannannivala@yahoo.com>

---
+++ java-basics/README.md
@@ -0,0 +1,11 @@
+# JAVA BASICS
+Project Module
+
+## Build and Run
+
+Linux/MacOS:
+
+```shell
+./gradlew jar
+java -jar build/libs/java-basics-1.0-SNAPSHOT.jar
+```
---
+++ java-basics/src/main/java/Lib.java
@@ -0,0 +1,12 @@
+public interface Lib {
+    static String getGreeting(){
+        int hour=java.time.LocalTime.now().getHour();
+        if(hour < 12){
+            return "Good Morning";
+        }else if(hour < 18){
+            return "Good Afternoon";
+        }else{
+            return "Good Evening";
+        }
+    }
+}
