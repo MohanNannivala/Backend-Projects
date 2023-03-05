005e457e568e5a920d7d8a557c1649bab77487e3
MohanNannivala <mohannannivala@yahoo.com>
2023-03-05 20:10:36 +0530

    models and entities created, also changed the spring boot version
    Signed-off-by: MohanNannivala <mohannannivala@yahoo.com>

---
+++ spring-taskmgr-v2/src/main/java/com/example/springtaskmgrv2/entities/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.springtaskmgrv2.entities;
+
+import javax.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private  Integer id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdAt;
+}
